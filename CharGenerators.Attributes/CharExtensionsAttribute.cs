4f015dfdf1756ff039089ab10b267a03e37af8d5
Adam Radocz <adam.radocz@gmail.com>
2022-03-10 21:51:46 +0100

    Add comments.

--- CharGenerators.Attributes/CharExtensionsAttribute.cs
+++ CharGenerators.Attributes/CharExtensionsAttribute.cs
@@ -1,10 +1,18 @@
-﻿namespace CharGenerators.Attributes
+namespace CharGenerators.Attributes
 {
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false)]
+    [System.Diagnostics.Conditional("CHARGENERATORS_USAGES")]
     public class CharExtensionsAttribute : System.Attribute
     {
+        /// <summary>
+        /// Genereate the switch for the listed characters.
+        /// </summary>
         public string OptimizeFor { get; set; }
 
+        /// <summary>
+        /// If set to <c>true</c>, the <c>ToStringFast</c> extension method will be created.
+        /// If set to <c>false</c>, a <c>private string CharToStringFast(char value)</c> class specific method will be created.
+        /// </summary>
         public bool Global { get; set; }
     }
 }
